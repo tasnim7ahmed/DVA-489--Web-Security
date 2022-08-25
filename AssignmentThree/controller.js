71003e9e747c5e09b16e88af994fa07ccc11c846
Tasnim Ahmed <tasnimahmed@iut-dhaka.edu>
2022-08-26 00:24:23 +0600

    squeak read added

--- AssignmentThree/controller.js
+++ AssignmentThree/controller.js
@@ -88,11 +88,26 @@ const getHome = (req, res) => {
   console.log("In getHome");
   squeaks = fs.readFileSync("squeaks", { encoding: "utf-8" });
   squeaks = JSON.parse(String(squeaks));
-  console.log(JSON.stringify(squeaks["2"]));
+  posts = "";
+  for (let i in squeaks) {
+    post = `<div class="card mb-2">
+    <div class="card-header">
+      ${squeaks[i]["username"]}
+      <span class="float-right">${squeaks[i]["time"]}</span>
+    </div>
+    <div class="card-body">
+      <p class="card-text">
+      ${squeaks[i]["post"]}
+      </p>
+    </div>
+  </div>`;
+    posts += post;
+  }
   var cookie = req.cookies.squeak_session;
   console.log(cookie.username);
   fs.readFile(__dirname + "/public/home.html", "utf8", (err, text) => {
     text = text.replace("{{currentUser}}", cookie.username);
+    text = text.replace("{{posts}}", posts);
     res.send(text);
   });
 };
