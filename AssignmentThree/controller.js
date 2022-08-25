95dffa87c5153e96cd50374633bd11e6d7d68854
Tasnim Ahmed <tasnimahmed@iut-dhaka.edu>
2022-08-26 01:55:25 +0600

    final product

--- AssignmentThree/controller.js
+++ AssignmentThree/controller.js
@@ -99,7 +99,7 @@ const getHome = (req, res) => {
     post = `<div class="card mb-2">
     <div class="card-header">
       ${squeaks[keys[i]]["username"]}
-      <span class="float-rkey[g]ht">${squeaks[keys[i]]["time"]}</span>
+      <span class="float-right">${squeaks[keys[i]]["time"]}</span>
     </div>
     <div class="card-body">
       <p class="card-text">
@@ -119,12 +119,46 @@ const getHome = (req, res) => {
 };
 
 const postHome = (req, res) => {
-  //const { squeak } = req.body
+  const { squeak } = req.body;
   //console.log(squeak)
   var cookie = req.cookies.squeak_session;
 
   squeaks = fs.readFileSync("squeaks", { encoding: "utf-8" });
+  squeaks = JSON.parse(String(squeaks));
+  keys = [];
+  for (let i in squeaks) {
+    keys.push(i);
+  }
+  keys = keys.reverse();
+  post_id = parseInt(keys[0]) + 1;
+  if (keys.length == 0) {
+    post_id = 1;
+  }
+  console.log(post_id);
+  post_id = post_id.toString();
+  console.log(post_id);
 
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+  let prnDt = new Date().toLocaleTimeString("en-us", options);
+
+  console.log(prnDt);
+
+  squeaks[post_id] = {
+    username: cookie.username,
+    post: squeak,
+    time: prnDt,
+  };
+
+  fs.writeFileSync("squeaks", JSON.stringify(squeaks));
   res.redirect("/home");
 };
 
